817827a49188acc0132ff4a60addf18ede008a21
Pranoy Dutta <pranoydll@gmail.com>
2017-08-21 21:27:08 -0500

    improved Landing page and fixed RSS Feed as well as README.md

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
     <h1>The Standard Students Podcast</h1>
+    <p>Check us out on <a target="_blank" href="https://itunes.apple.com/us/podcast/standard-students-podcast/id1273635244?mt=2">Itunes!</a></p>
+    <div class="menu">
+      <a class="menu-item" href="/blog">Blog</a>
+      <a class="menu-item" href="/archive">Archive</a>
+      <a class="menu-item" href="/about">About</a>
+    </div>
   </div>
 </template>
 
 <style media="screen" scoped>
-  h1{
+  h1 {
     text-decoration: none;
     font-weight: normal;
     text-align: center;
+  }
+  p {
+    text-align: center;
+    font-size: 20px;
+  }
+  .menu {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  .menu-item {
+    font-size: 20px;
   }
 </style>
